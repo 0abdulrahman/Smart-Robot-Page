4b7e2f450b95f3a718f0439997880f8f6b848dcc
7saru <saboo9653@gmail.com>
2023-04-06 23:41:48 +0200

    making the page responsive

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const button = document.querySelector("#scrollTop");
+const navmenu = document.querySelector("header .container nav");
+const navBtn = document.querySelector("#nav-btn");
+const navLis = document.querySelectorAll("header nav li a");
 
 function scrollToTop() {
   window.scrollTo({
     top: 0,
   });
 }
-
 button.addEventListener("click", scrollToTop);
 
 function checkScroll() {
@@ -15,5 +17,18 @@ function checkScroll() {
     button.style.cssText = "transform: translateX(100px);";
   }
 }
-
 window.addEventListener("scroll", checkScroll);
+
+navBtn.addEventListener("click", () => {
+  navmenu.classList.toggle("active");
+  navBtn.classList.toggle("fa-xmark");
+  navBtn.classList.toggle("fa-bars");
+});
+
+navLis.forEach((el) => {
+  el.addEventListener("click", () => {
+    navmenu.classList.toggle("active");
+    navBtn.classList.toggle("fa-xmark");
+    navBtn.classList.toggle("fa-bars");
+  });
+});
